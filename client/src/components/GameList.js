c6d406b6cee56af9b4d602aa183e6bf05f39ff3b
DESKTOP-REI9PEB\kosmo <nahs001@naver.com>
2023-07-22 19:13:06 +0900

    7/22 채팅방 리스트페이지 구현

--- client/src/components/GameList.js
+++ client/src/components/GameList.js
@@ -8,6 +8,8 @@ const GameList = () => {
     const [rooms,setRooms] = useState([]);
     const [isReady,setIsReady] = useState(false);
     const [selectedRooms,setSelectedRooms] = useState([]);
+    const [selectedOption, setSelectedOption] = useState('room_name');
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         const getList = async () => {
@@ -31,14 +33,157 @@ const GameList = () => {
 
     },[isReady]);
 
+    // 전체 방 보기 클릭 시
+    const selectAllRooms = () => {
+        if(rooms){
+            setSelectedRooms(rooms);
+        }
+    }
+
+    // 모집중인 방만 보기 클릭 시
+    const selectRecruitRooms =() => {
+        if(rooms){
+            let recruitRooms = [];
+
+            rooms.forEach(room => {
+                if(room.max_member>room.curr_member){
+                    recruitRooms.push(room);
+                }
+            });
+
+            setSelectedRooms(recruitRooms);
+        }
+    }
+
+    // 관전가능한 방만 보기 클릭 시
+    const selectWatchableRooms =() => {
+        if(rooms){
+            let watchableRooms = [];
+
+            rooms.forEach(room => {
+                if(room.watching===0){
+                    watchableRooms.push(room);
+                }
+            });
+
+            setSelectedRooms(watchableRooms);
+        }
+    }
+
+    // 영상, 음성 버튼 클릭 시
+    const setVideoAudio = (selected) => {
+        if(selected==="video"){
+            if(rooms){
+                let selected = [];
+    
+                rooms.forEach(room => {
+                    if(room.video===0){
+                        selected.push(room);
+                    }
+                });
+    
+                setSelectedRooms(selected);
+            }
+        }else if(selected==="audio"){
+            if(rooms){
+                let selected = [];
+    
+                rooms.forEach(room => {
+                    if(room.video===1){
+                        selected.push(room);
+                    }
+                });
+    
+                setSelectedRooms(selected);
+            }
+        }
+    }
+
+    // 해시태그 클릭 시
+    const selectHashtag = (selectedTag) => {
+        if(rooms){
+            let selected = [];
+
+            rooms.forEach(room => {
+                if(room.tag_contents){
+                    room.tag_contents.forEach(tag => {
+                        if(selectedTag === tag){
+                            selected.push(room);
+                        }
+                    });
+                }
+            });
+
+            setSelectedRooms(selected);
+        }
+    }
+
+    // 방 만들기 클릭 시
+    const createRoom = () => {
+        window.parent.location.href = "https://192.168.0.68:8080/game/play/room_create";
+    }
+
+    // 방 상세페이지로 이동
+    const goGameDetail = (room_num) => {
+        window.parent.location.href = "https://192.168.0.68:8080/game/play/room_detail?room_num="+room_num;
+    }
+
+    // 검색창 셀렉트태그 변경 시
+    const handleOptionChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+
+    // 검색창 input에 값 변경 시
+    const handleInputChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    // 검색창 submit 시
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if(selectedOption==="room_name"){
+            if(rooms){
+                let selected = [];
+    
+                rooms.forEach(room => {
+                    if(room.room_name && room.room_name.includes(searchTerm)){
+                        selected.push(room);
+                    }
+                });
+    
+                setSelectedRooms(selected);
+            }
+        }else if(selectedOption==="nickname"){
+            if(rooms){
+                let selected = [];
+    
+                rooms.forEach(room => {
+                    if(room.nickname && room.nickname.includes(searchTerm)){
+                        selected.push(room);
+                    }
+                });
+    
+                setSelectedRooms(selected);
+            }
+        }
+    }
+
+    // input 태그에서 엔터 누르면 handleSubmit 호출
+    const enterKeyPress = (evt) => {
+        if(evt.key === "Enter"){
+            handleSubmit(evt)
+        }
+    }
+
 
     return (
         <div>
-            <button>방 만들기</button>
+            <button onClick={createRoom}>방 만들기</button>
             <br/>
-            <button>전체 방 보기</button>
-            <button>모집중인 방만 보기</button>
-            <button>관전가능한 방만 보기</button>
+            <button onClick={selectAllRooms}>전체 방 보기</button>
+            <button onClick={selectRecruitRooms}>모집중인 방만 보기</button>
+            <button onClick={selectWatchableRooms}>관전가능한 방만 보기</button>
 
             <table>
                 <thead>
@@ -46,8 +191,17 @@ const GameList = () => {
                     <td>이름 및 소개</td>
                     <td>게임정보</td>
                 </thead>
-                {selectedRooms && selectedRooms.sort((a, b) => new Date(b.create_date) - new Date(a.create_date)).map(room => <GameRoom key={room.room_num} room={room} />)}
+                {selectedRooms && selectedRooms.sort((a, b) => new Date(b.create_date) - new Date(a.create_date)).map(room => 
+                <GameRoom key={room.room_num} room={room} setVideoAudio={setVideoAudio} selectHashtag={selectHashtag} goGameDetail={goGameDetail}/>)}
             </table>
+            <form onSubmit={handleSubmit}>
+                <select defaultValue={"room_name"} onChange={handleOptionChange}>
+                    <option value={"nickname"}>작성자</option>
+                    <option value={"room_name"}>제목</option>
+                </select>
+                <input type="text" onChange={handleInputChange} onKeyDown={enterKeyPress}></input>
+                <button>검색</button>
+            </form>
         </div>
     )
 }
--- client/src/components/GameRoom.js
+++ client/src/components/GameRoom.js
@@ -3,31 +3,43 @@
 const GameRoom = (props) => {
     const room = props.room;
 
+    const setVideoAudio = props.setVideoAudio;
 
-    return (
-        <tr>
-            <td>
-                {room.nickname}
-            </td>
-            <td>
-                <h4>{room.room_name}</h4>
-                <br/>
-                <span>{room.plot}</span>
-            </td>
-            <td>
-                {room.tag && room.tag.map(tag_content => <span className="hashtag">{tag_content}</span>)}
-                <br/>
-                <span>현재인원 : {room.curr_member}/{room.max_member}</span>
-                <br/>
-                <span>다음 게임 일시 : {!room.next_play_date && "미정"}{room.next_play_date && room.next_play_date}</span>
-                <br/>
-                <span>예상 기간 : {!room.period && "미정"}{room.period && room.period+"주"}</span>
-                <br/>
-                {room.video===0 && <span className="video">영상</span>}
-                {room.video===1 && <span className="audio">음성</span>}
-            </td>
-        </tr>
-    )
+    const selectHashtag = props.selectHashtag;
+
+    const goGameDetail = props.goGameDetail;
+
+    if(room){
+        return (
+        
+            <tr>
+                <td>
+                    {room.nickname}
+                </td>
+                <td>
+                    <a onClick={() => goGameDetail(room.room_num)}>
+                    <h3>{room.room_name}</h3>
+                    <br/>
+                    <span>{room.plot}</span>
+                    </a>
+                </td>
+                <td>
+                    {room.tag_contents && room.tag_contents.map(tag_content => <button className="hashtag" onClick={() => selectHashtag(tag_content)}>{tag_content}</button>)}
+                    <br/>
+                    <span>현재인원 : {room.curr_member}/{room.max_member}</span>
+                    <br/>
+                    <span>다음 게임 일시 : {!room.next_play_date && "미정"}{room.next_play_date && room.next_play_date}</span>
+                    <br/>
+                    <span>예상 기간 : {!room.period && "미정"}{room.period && room.period+"주"}</span>
+                    <br/>
+                    {room.video===0 && <button className="video" onClick={() => setVideoAudio("video")}>영상</button>}
+                    {room.video===1 && <button className="audio" onClick={() => setVideoAudio("audio")}>음성</button>}
+                </td>
+            </tr>
+            
+        )
+    }
+    
 }
 
 export default GameRoom;
