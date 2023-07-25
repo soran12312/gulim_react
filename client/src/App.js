355a8340b77fd214be8162621ea61d429ee5572c
DESKTOP-8MQ3HFK\kosmo <dbswowls100@naver.com>
2023-07-25 09:31:30 +0900

    판매

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import VideoRoom from './components/VideoRoom';
 import GameList from "./components/GameList";
+import Sales from "./components/Sales";
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
       <Routes>
           <Route path="/:room/:userId" element={<VideoRoom />}/>
           <Route path="/roomList" element={<GameList/>}/>
+          <Route path="/sales" element={<Sales/>}/>
       </Routes>
     </BrowserRouter>
   );
